96211895f23881be47bef2edce7c5cccf6e76dec
zhengchengyy <zhengchengyy@qq.com>
2019-02-26 17:03:20 +0800

    add feature extrator

---
+++ featureExtrator/FeatureExtratorWithDB.py
@@ -0,0 +1,60 @@
+from FeatureModules import VarianceModule
+from feature_extractor import FeatureExtractor
+import time
+
+from pymongo import MongoClient
+from exceptions import CollectionError
+
+
+config = {'action':'uneasy',
+          'db':'beaglebone',
+          'tag_collection':'tags_5',
+          'volt_collection':'volts_5'}
+
+def read_from_db(action, db, volt_collection, tag_collection,port=27017, host='localhost', ndevices=3):
+    client = MongoClient(port=port, host=host)
+    database = client[db]
+    tag_collection = database[tag_collection]
+    volt_collection = database[volt_collection]
+
+    try:
+        if volt_collection.count_documents({}) + tag_collection.count_documents({}) < 2:
+            raise CollectionError('Collection not found, please check names of the collection and database')
+    except CollectionError as e:
+        print(e.message)
+
+    ntags = tag_collection.count_documents({'tag':action})
+
+    # read the data that is of a certain action one by one
+    for tag in tag_collection.find({'tag': action}):
+        inittime, termtime = tag['inittime'], tag['termtime']
+
+        # get the arrays according to which we will plot later
+        times, volts = {}, {}
+        for i in range(1, ndevices + 1):
+            times[i] = []
+            volts[i] = []
+
+        for volt in volt_collection.find({'time': {'$gt': inittime,'$lt': termtime}}):
+            device_no = int(volt['device_no'])
+            v = volt['voltage']
+            time = volt['time']
+            times[device_no].append(time)
+            volts[device_no].append(v)
+
+    return times,volts
+
+
+if __name__=='__main__':
+    times, volts= read_from_db(action=config['action'],
+                               db=config['db'],
+                               tag_collection=config['tag_collection'],
+                               volt_collection=config['volt_collection'])
+
+    variancemodule = VarianceModule(5, 3)
+    extractor = FeatureExtractor()
+    extractor.register(variancemodule)
+    for volt in volts[1]:
+        #print(volt)
+        time.sleep(1)
+        extractor.process(volt)
---
+++ featureExtrator/FeatureModules.py
@@ -0,0 +1,28 @@
+from feature_extractor import SumModule
+from feature_extractor import FeatureExtractor
+from feature_extractor import ProcessModule
+
+
+class AverageModule(ProcessModule):
+    """功能是对满队列中的所有数据求平均值。返回平均值。
+    表示震动幅度的平均情况"""
+    # 优化，不用重复计算值，只计算增加和减少的数据；可以从其它组件获取数据
+
+    def processFullQueue(self):
+        for value in self.queue.queue:
+            sum = sum + value
+        return sum/self.queue.maxsize
+
+class VarianceModule(ProcessModule):
+    """功能是对满队列中的所有数据求方差。返回方差。
+    表示震动频率的平均情况"""
+
+    def processFullQueue(self):
+        sum = 0
+        variance = 0
+        for value in self.queue.queue:
+            sum = sum + value
+        average = sum/self.queue.maxsize
+        for value in self.queue.queue:
+            variance = variance + (value - average) ** 2
+        return variance/self.queue.maxsize
