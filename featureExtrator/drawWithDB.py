7d456b880c502f0e9546e775dff34edb8c63b97b
zhengchengyy <zhengchengyy@qq.com>
2019-07-10 09:22:50 +0800

    add fft and cwt

--- featureExtrator/drawWithDB.py
+++ featureExtrator/drawWithDB.py
@@ -4,13 +4,11 @@ from matplotlib import style
 from exceptions import CollectionError
 import time
 
-import numpy as np
-import pywt
-
-config = {'action':'turn_over_left',
+config = {'action':'turn_over',
           'db':'beaglebone',
-          'tag_collection':'tags_531',
-          'volt_collection':'volts_531'}
+          'tag_collection':'tags_411',
+          'volt_collection':'volts_411',
+          'offset':0}
 
 def timeToFormat(t):
     ftime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(t))
@@ -20,7 +18,7 @@ def timeToSecond(t):
     stime = time.strftime("%M:%S", time.localtime(t))
     return stime
 
-def plot_from_db(action, db, volt_collection, tag_collection,port=27017, host='localhost', ndevices=3):
+def plot_from_db(action, db, volt_collection, tag_collection, port=27017, host='localhost', ndevices=5, offset=0):
     client = MongoClient(port=port, host=host)
     database = client[db]
     tag_collection = database[tag_collection]
@@ -42,7 +40,7 @@ def plot_from_db(action, db, volt_collection, tag_collection,port=27017, host='l
     # plot the data that is of a certain action one by one
     for tag in tag_collection.find({'tag': action}):
         # inittime
-        inittime, termtime = tag['inittime']-95432238.30212903, tag['termtime']-95432238.30212903
+        inittime, termtime = tag['inittime']-offset, tag['termtime']-offset
         # get the arrays according to which we will plot later
         times, volts = {}, {}
         for i in range(1, ndevices + 1):
@@ -57,14 +55,14 @@ def plot_from_db(action, db, volt_collection, tag_collection,port=27017, host='l
             volts[device_no].append(v)
 
         style.use('default')
-        colors = ['r', 'b', 'g', 'c', 'm']
+        colors = ['r', 'b', 'g', 'c', 'm']   #m c
         subtitle = ['A', 'B', 'C' ,'D', 'E', 'F', 'G']
         base = ntags*100+10
 
         # plot, add_subplot(211)将画布分割成2行1列，图像画在从左到右从上到下的第1块
         ax = fig.add_subplot(base+n)
         plt.subplots_adjust(hspace=0.5)  # 函数中的wspace是子图之间的垂直间距，hspace是子图的上下间距
-        ax.set_title("Person"+subtitle[n-1]+": "+timeToFormat(inittime)+" ~ "+timeToFormat(termtime))
+        ax.set_title("Person"+subtitle[n-1]+": "+timeToFormat(inittime+offset)+" ~ "+timeToFormat(termtime+offset))
         ax.set_xlim(inittime, termtime)
 
         # 自定义y轴的区间范围，可以使图放大或者缩小
@@ -73,16 +71,9 @@ def plot_from_db(action, db, volt_collection, tag_collection,port=27017, host='l
         # ax.set_ylim([0.82, 0.83])
         ax.set_ylabel('voltage')
 
-        for i in range(1, 1 + 1):
+        for i in range(1, ndevices + 1):
             # [v + i*0.2 for v in volts[i]]为了把多个设备的数据隔离开
-            # ax.plot(times[i], volts[i], label='device_' + str(i), color=colors[i - 1], alpha=0.9)
-            # t = np.linspace(0, 1, 400, endpoint=False)
-            cwtmatr1, freqs1 = pywt.cwt(volts[i], np.arange(1, len(volts[i])), 'cgau8', 1 / 400)
-            ax.contourf(times[i], freqs1, abs(cwtmatr1))
-            # ax.title('time-frequency relationship of signal_1')
-            # ax.xlabel('Time/second')
-            # ax.ylabel('Frequency/Hz')
-
+            ax.plot(times[i], volts[i], label='device_' + str(i), color=colors[i - 1], alpha=0.9)
 
         if n  == 1:
             ax.legend(loc='upper right')
@@ -97,7 +88,7 @@ def plot_from_db(action, db, volt_collection, tag_collection,port=27017, host='l
         interval = length // 10 - 1
         for i in range(0,length,interval):
             xticks.append(times[1][i])
-            xticklabels.append(timeToSecond(times[1][i]))
+            xticklabels.append(timeToSecond(times[1][i]+offset))
         ax.set_xticks(xticks)  # 设定标签的实际数字，数据类型必须和原数据一致
         ax.set_xticklabels(xticklabels, rotation=15)  # 设定我们希望它显示的结果，xticks和xticklabels的元素一一对应
 
@@ -108,4 +99,5 @@ if __name__=='__main__':
     plot_from_db(action=config['action'],
                   db=config['db'],
                   tag_collection=config['tag_collection'],
-                  volt_collection=config['volt_collection'])
+                  volt_collection=config['volt_collection'],
+                  offset=config['offset'])
