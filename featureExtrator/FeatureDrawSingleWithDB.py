5f59e0e104a86740c5e1782e6dc0f3c11fee17a7
zhengchengyy <zhengchengyy@qq.com>
2019-07-22 16:06:03 +0800

    draw features with database

--- featureExtrator/FeatureDrawSingleWithDB.py
+++ featureExtrator/FeatureDrawSingleWithDB.py
@@ -9,13 +9,12 @@ from pymongo import MongoClient
 from matplotlib import pyplot as plt
 from matplotlib import style
 
-config = {'action': 'turn_over',
-          'db': 'beaglebone',
-          'tag_collection': 'tags_411',
-          'volt_collection': 'volts_411'}
+config = {'action':'legs_stretch',
+          'db':'beaglebone',
+          'tag_collection':'tags_411',
+          'volt_collection':'volts_411'}
 
-
-def read_from_db(action, db, volt_collection, tag_collection, port=27017, host='localhost', ndevices=5):
+def read_from_db(action, db, volt_collection, tag_collection,port=27017, host='localhost', ndevices=5):
     client = MongoClient(port=port, host=host)
     database = client[db]
     tag_collection = database[tag_collection]
@@ -27,7 +26,7 @@ def read_from_db(action, db, volt_collection, tag_collection, port=27017, host='
     except CollectionError as e:
         print(e.message)
 
-    ntags = tag_collection.count_documents({'tag': action})
+    ntags = tag_collection.count_documents({'tag':action})
 
     # read the data that is of a certain action one by one
     for tag in tag_collection.find({'tag': action}):
@@ -39,17 +38,25 @@ def read_from_db(action, db, volt_collection, tag_collection, port=27017, host='
             times[i] = []
             volts[i] = []
 
-        for volt in volt_collection.find({'time': {'$gt': inittime, '$lt': termtime}}):
+        for volt in volt_collection.find({'time': {'$gt': inittime,'$lt': termtime}}):
             device_no = int(volt['device_no'])
             v = volt['voltage']
             time = volt['time']
             times[device_no].append(time)
             volts[device_no].append(v)
 
-    return times, volts
+    return times,volts
+
+def draw_from_data(feature_type, feature_time, feature_values, ndevices=5):
+    style.use('default')
+    colors = ['r', 'b', 'g', 'c', 'm']  # m c
+    for i in range(1, ndevices + 1):
+        plt.plot(feature_times[i], feature_values[i]['Variance'], label='device_' + str(i), color=colors[i - 1],
+                 alpha=0.9)
+    plt.show()
 
 
-if __name__ == '__main__':
+if __name__=='__main__':
     times, volts = read_from_db(action=config['action'],
                                 db=config['db'],
                                 tag_collection=config['tag_collection'],
@@ -57,8 +64,8 @@ if __name__ == '__main__':
 
     # 根据时间采集数据，基本单位为s，比如1s、10s、30s、60s
     # interval表示每次分析的时间跨度，rate表示间隔多长时间进行一次分析
-    interval = 2
-    rate = 2
+    interval = 1
+    rate = 1
 
     # 定义特征提取器
     extractor = FeatureExtractor()
@@ -81,29 +88,35 @@ if __name__ == '__main__':
     # 定义设备数
     ndevices = 5
 
-    # 定义存储特征列表
-    features = {}
+    # 定义特征数量
+    nfeatures = 3
+
+    # 定义存储时间、特征列表
+    feature_times, feature_values = {}, {}
     for i in range(1, ndevices + 1):
-        features[i] = []
+        feature_times[i] = []
+        feature_values[i] = {'Variance':[],'Average':[],'ThresholdCounter':[]}
 
     # 对每个采集设备进行特征提取
     for i in range(1, ndevices + 1):
         for j in range(len(volts[i])):
             value = {
-                "time": times[i][j],
-                "volt": volts[i][j]
+                "time" : times[i][j],
+                "volt" : volts[i][j]
             }
             output = extractor.process(value)
             if (output):
-                feature = {
-                    # "device_no": i,
+                features = {
+                    "device_no": i,
                     "feature_time": times[i][j],
-                    "feature_value": output['Variance'],
-                    # "interval": interval,
-                    # "rate": rate
+                    "feature_value": output,
+                    "interval": interval,
+                    "rate": rate
                 }
-                # print(feature)
-                features[i].append(feature)
+                feature_times[i].append(features['feature_time'])
+                for feature_type in feature_values[i].keys():
+                    feature_values[i][feature_type].append(features['feature_value'][feature_type])
+
                 # 把特征数据存入数据库
                 # collection.insert_one(features)
 
@@ -112,35 +125,31 @@ if __name__ == '__main__':
         averagemodule.clear()
         thresholdcounter.clear()
 
-    title = "features"
+    title = config['volt_collection'][6:] + "" + config['action']
     fig = plt.figure(title, figsize=(6, 8))
-    fig.suptitle("feature")
+    fig.suptitle("features")
 
-    feature_count = 3
+    # 定义画布位置的计数
     n = 1
 
-    style.use('default')
-    colors = ['r', 'b', 'g', 'c', 'm']  # m c
-    subtitle = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
-    # base = feature_count * 100 + 10
+    for feature_type in feature_values[1].keys():
+        style.use('default')
+        colors = ['r', 'b', 'g', 'c', 'm']  # m c
+        base = nfeatures * 100 + 10
+        # plot, add_subplot(211)将画布分割成2行1列，图像画在从左到右从上到下的第1块
+        ax = fig.add_subplot(base + n)
+        plt.subplots_adjust(hspace=0.5)  # 函数中的wspace是子图之间的垂直间距，hspace是子图的上下间距
+        ax.set_title(feature_type)
+        # ax.set_xlim(inittime, termtime)
 
-    # plot, add_subplot(211)将画布分割成2行1列，图像画在从左到右从上到下的第1块
-    # ax = fig.add_subplot(base + n)
-    # plt.subplots_adjust(hspace=0.5)  # 函数中的wspace是子图之间的垂直间距，hspace是子图的上下间距
-    # ax.set_xlim(inittime, termtime)
+        for i in range(1, ndevices + 1):
+            ax.plot(feature_times[i], feature_values[i][feature_type],
+                    label='device_' + str(i), color=colors[i - 1], alpha=0.9)
 
-    # 定义存储时间、特征列表
-    feature_times, feature_values = {}, {}
-    for i in range(1, ndevices + 1):
-        feature_times[i] = []
-        feature_values[i] = []
-    for i in range(1, len(features)+1):
-        for j in range(len(features[i])):
-            feature_times[i].append(features[i][j]['feature_time'])
-            feature_values[i].append(features[i][j]['feature_value'])
+        if n  == 1:
+            ax.legend(loc='upper right')
+        if n == nfeatures:
+            ax.set_xlabel('Time')
+        n += 1
 
-    # print(feature_values)
-    # print(feature_times)
-    for i in range(1, ndevices + 1):
-        plt.plot(feature_times[i], feature_values[i], label='device_' + str(i), color=colors[i - 1], alpha=0.9)
     plt.show()
--- featureExtrator/test/FeatureDrawSingleWithDB.py
+++ featureExtrator/test/FeatureDrawSingleWithDB.py
@@ -9,12 +9,13 @@ from pymongo import MongoClient
 from matplotlib import pyplot as plt
 from matplotlib import style
 
-config = {'action':'legs_stretch',
-          'db':'beaglebone',
-          'tag_collection':'tags_411',
-          'volt_collection':'volts_411'}
+config = {'action': 'turn_over',
+          'db': 'beaglebone',
+          'tag_collection': 'tags_411',
+          'volt_collection': 'volts_411'}
 
-def read_from_db(action, db, volt_collection, tag_collection,port=27017, host='localhost', ndevices=5):
+
+def read_from_db(action, db, volt_collection, tag_collection, port=27017, host='localhost', ndevices=5):
     client = MongoClient(port=port, host=host)
     database = client[db]
     tag_collection = database[tag_collection]
@@ -26,7 +27,7 @@ def read_from_db(action, db, volt_collection, tag_collection,port=27017, host='l
     except CollectionError as e:
         print(e.message)
 
-    ntags = tag_collection.count_documents({'tag':action})
+    ntags = tag_collection.count_documents({'tag': action})
 
     # read the data that is of a certain action one by one
     for tag in tag_collection.find({'tag': action}):
@@ -38,25 +39,17 @@ def read_from_db(action, db, volt_collection, tag_collection,port=27017, host='l
             times[i] = []
             volts[i] = []
 
-        for volt in volt_collection.find({'time': {'$gt': inittime,'$lt': termtime}}):
+        for volt in volt_collection.find({'time': {'$gt': inittime, '$lt': termtime}}):
             device_no = int(volt['device_no'])
             v = volt['voltage']
             time = volt['time']
             times[device_no].append(time)
             volts[device_no].append(v)
 
-    return times,volts
-
-def draw_from_data(feature_type, feature_time, feature_values, ndevices=5):
-    style.use('default')
-    colors = ['r', 'b', 'g', 'c', 'm']  # m c
-    for i in range(1, ndevices + 1):
-        plt.plot(feature_times[i], feature_values[i]['Variance'], label='device_' + str(i), color=colors[i - 1],
-                 alpha=0.9)
-    plt.show()
+    return times, volts
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     times, volts = read_from_db(action=config['action'],
                                 db=config['db'],
                                 tag_collection=config['tag_collection'],
@@ -64,8 +57,8 @@ if __name__=='__main__':
 
     # 根据时间采集数据，基本单位为s，比如1s、10s、30s、60s
     # interval表示每次分析的时间跨度，rate表示间隔多长时间进行一次分析
-    interval = 1
-    rate = 1
+    interval = 2
+    rate = 2
 
     # 定义特征提取器
     extractor = FeatureExtractor()
@@ -88,35 +81,29 @@ if __name__=='__main__':
     # 定义设备数
     ndevices = 5
 
-    # 定义特征数量
-    nfeatures = 3
-
-    # 定义存储时间、特征列表
-    feature_times, feature_values = {}, {}
+    # 定义存储特征列表
+    features = {}
     for i in range(1, ndevices + 1):
-        feature_times[i] = []
-        feature_values[i] = {'Variance':[],'Average':[],'ThresholdCounter':[]}
+        features[i] = []
 
     # 对每个采集设备进行特征提取
     for i in range(1, ndevices + 1):
         for j in range(len(volts[i])):
             value = {
-                "time" : times[i][j],
-                "volt" : volts[i][j]
+                "time": times[i][j],
+                "volt": volts[i][j]
             }
             output = extractor.process(value)
             if (output):
-                features = {
-                    "device_no": i,
+                feature = {
+                    # "device_no": i,
                     "feature_time": times[i][j],
-                    "feature_value": output,
-                    "interval": interval,
-                    "rate": rate
+                    "feature_value": output['Variance'],
+                    # "interval": interval,
+                    # "rate": rate
                 }
-                feature_times[i].append(features['feature_time'])
-                for feature_type in feature_values[i].keys():
-                    feature_values[i][feature_type].append(features['feature_value'][feature_type])
-
+                # print(feature)
+                features[i].append(feature)
                 # 把特征数据存入数据库
                 # collection.insert_one(features)
 
@@ -125,31 +112,35 @@ if __name__=='__main__':
         averagemodule.clear()
         thresholdcounter.clear()
 
-    title = config['volt_collection'][6:] + "" + config['action']
+    title = "features"
     fig = plt.figure(title, figsize=(6, 8))
-    fig.suptitle("features")
+    fig.suptitle("feature")
 
-    # 定义画布位置的计数
+    feature_count = 3
     n = 1
 
-    for feature_type in feature_values[1].keys():
-        style.use('default')
-        colors = ['r', 'b', 'g', 'c', 'm']  # m c
-        base = nfeatures * 100 + 10
-        # plot, add_subplot(211)将画布分割成2行1列，图像画在从左到右从上到下的第1块
-        ax = fig.add_subplot(base + n)
-        plt.subplots_adjust(hspace=0.5)  # 函数中的wspace是子图之间的垂直间距，hspace是子图的上下间距
-        ax.set_title(feature_type)
-        # ax.set_xlim(inittime, termtime)
+    style.use('default')
+    colors = ['r', 'b', 'g', 'c', 'm']  # m c
+    subtitle = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+    # base = feature_count * 100 + 10
 
-        for i in range(1, ndevices + 1):
-            ax.plot(feature_times[i], feature_values[i][feature_type],
-                    label='device_' + str(i), color=colors[i - 1], alpha=0.9)
+    # plot, add_subplot(211)将画布分割成2行1列，图像画在从左到右从上到下的第1块
+    # ax = fig.add_subplot(base + n)
+    # plt.subplots_adjust(hspace=0.5)  # 函数中的wspace是子图之间的垂直间距，hspace是子图的上下间距
+    # ax.set_xlim(inittime, termtime)
 
-        if n  == 1:
-            ax.legend(loc='upper right')
-        if n == nfeatures:
-            ax.set_xlabel('Time')
-        n += 1
+    # 定义存储时间、特征列表
+    feature_times, feature_values = {}, {}
+    for i in range(1, ndevices + 1):
+        feature_times[i] = []
+        feature_values[i] = []
+    for i in range(1, len(features)+1):
+        for j in range(len(features[i])):
+            feature_times[i].append(features[i][j]['feature_time'])
+            feature_values[i].append(features[i][j]['feature_value'])
 
+    # print(feature_values)
+    # print(feature_times)
+    for i in range(1, ndevices + 1):
+        plt.plot(feature_times[i], feature_values[i], label='device_' + str(i), color=colors[i - 1], alpha=0.9)
     plt.show()
